3b038b0de153a60ec3cfe61fdd821dcddec76cdd
deepika 9282 <deepika071221@gmail.com>
2026-03-23 18:29:22 +0545

    chore: commit current changes

--- backend/saralsewa/urls.py
+++ backend/saralsewa/urls.py
@@ -2,8 +2,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
+
+
+def root_status(_request):
+    return JsonResponse({
+        'message': 'Saral Sewa backend is running.',
+        'api': '/api/',
+        'admin': '/django-admin/',
+    })
 
 urlpatterns = [
+    path('', root_status, name='root-status'),
+    path('health/', root_status, name='health'),
     path('django-admin/', admin.site.urls),
     path('api/', include('core.urls')),
 ]
--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -28,7 +28,6 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
